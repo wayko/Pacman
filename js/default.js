f7b0dddf8269404e2b07b55f67c9c2a64964f395
Wayko <programmers@dreamcpu.com>
2019-10-09 16:54:30 -0400

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,116 +7,87 @@ ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
 
 const level1 = 
 [
-	//Top Portion
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
+	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .5, .5, 1, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 2],
+	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, .5, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Ghost and Pacman Portion
-	[8, 1, 0, 0, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 8],
+	
+	[8, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 8],
 	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
-	[8, 1, 0, 0, 1, 1, 8, 5, 6, 7, .7, 8, 1, 1, 1, 0, 0, 1, 8],
+	[8, 1, 0, 0, 1, 1, 8, 5, 6, 7, 2, 8, 1, 1, 1, 0, 0, 1, 8],
 	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
 	[8, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, .8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, .8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, .1, .1, .1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 4, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Bottom Portion
+	[8, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
+	
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[2, 1, .5, .5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 2],
+	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, .5, 1, 8],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, .5, .5, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
+	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .5, .5, 1, 2],
 	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[2, 2, 2, .2, .2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 const level2 =
 [
-	//Top Portion
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
+	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .5, .5, 1, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 8],
+	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, .5, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Ghost and Pacman Portion
-	[8, 1, 0, 0, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 8],
+	
+	[8, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 8],
 	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
-	[8, 1, 0, 0, 1, 1, 8, 5, 6, 7, .7, 8, 1, 1, 1, 0, 0, 1, 8],
+	[8, 1, 0, 0, 1, 1, 8, 5, 6, 7, 2, 8, 1, 1, 1, 0, 0, 1, 8],
 	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
 	[8, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, 8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, 8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 4, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Bottom Portion
+	[8, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
+	
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
 	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, .5, .5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 8],
+	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, .5, 1, 8],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
 	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, .5, .5, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
+	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .5, .5, 1, 2],
 	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[2, 2, 2, .2, .2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
  const level3 =
 [
-	//Top Portion
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[2, 1, .5, .5, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
-	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[8, 1, .5, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Ghost and Pacman Portion
-	[8, 1, 0, 0, 1, 1, 0, 0, 2, 2, 0, 0, 1, 1, 1, 0, 0, 1, 8],
-	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
-	[8, 1, 0, 0, 1, 1, 8, 5, 6, 7, .7, 8, 1, 1, 1, 0, 0, 1, 8],
-	[8, 1, 1, 1, 1, 1, 8, 2, 2, 2, 2, 8, 2, 1, 1, 1, 1, 1, 8],
-	[8, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, 8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 2, 8, 2, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	//Bottom Portion
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8],
-	[8, 1, .5, .5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .6, .6, 1, 8],
-	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, 9, 2, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .9, 2, .9, 2],
-	[2, 1, .5, .5, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, .6, .6, 1, 2],
-	[8, 3, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1, 3, 8],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[2, 2, 2, .2, .2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 function draw(e)
 {
-	
 	return e;
 }
+<<<<<<< HEAD
 
 
 
  function createGhost(ctx, x, y, fill, stroke, stroke2)
+=======
+draw(ctx)
+{
+	ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+	ctx.rect(0,0, GAME_WIDTH,GAME_HEIGHT);
+	ctx.fill();
+	createGame(level1, "rgb(0, 0, 255)");
+}
+function createGhost(ctx, x, y, fill, stroke, stroke2)
+>>>>>>> parent of 15cb67b... Fixed stage and added new ghost
 {
 	this.x = x;
 	this.y = y;
@@ -151,6 +122,7 @@ function draw(e)
 	ctx.stroke();
 	ctx.closePath();
 	ctx.fill();	
+	
 }
 
 function createBorders(level, borderFill)
@@ -162,166 +134,97 @@ function createBorders(level, borderFill)
 	{
 		row.forEach((border, borderIndex) => 
 		{
-			if(border === 0) //horizontal border
+			if(border === 0)
 			{
 				x = 38.7 * borderIndex;
 				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+15,y-25,38.7,13);
+				ctx.fillRect(x+15,y,38.7,13);
 				ctx.fillStyle = this.borderFill;
-				
-				ctx.fill();
 			}
-				
-			if(border === 8) //vertical  border
+			if(border === 8)
 			{
 				x = 40 * borderIndex;
 				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+15,y-32,16,40);
+				ctx.fillRect(x+15,y-7,16,40);
 				ctx.fillStyle = this.borderFill;
-				ctx.fill();
 			}
-			if(border === .8) //vertical  border
-			{
-				x = 40 * borderIndex;
-				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+35,y-32,30,40);
-				ctx.fillStyle = this.borderFill;
-				ctx.fill();
-			}
-			if(border === .1) //horizontal border
-			{
-				x = 38.7 * borderIndex;
-				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+35,y-25,38.7,13);
-				ctx.fillStyle = this.borderFill;
-				
-				ctx.fill();
-			}
-			if(border === .5) //inner horizontal bars
+			if(border === .5)
 			{
 				x = 30 * borderIndex;
 				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x,y-25,46,13);
+				ctx.fillRect(x,y,38.7,13);
 				ctx.fillStyle = this.borderFill;
-				
-				ctx.fill();
 				
 			}	
-			if(border === .6) //inner horizontal bars
+			if(border === 9)
 			{
 				x = 30 * borderIndex;
 				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+165,y-25,46,13);
+				ctx.fillRect(x,y-7,16,40);
 				ctx.fillStyle = this.borderFill;
 				
-				ctx.fill();
-				
 			}
-			if(border === 9) //inner vertical bars left
+				
+			if(border === .9)
 			{
 				x = 30 * borderIndex;
 				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x,y-32,16,40);
-				ctx.fillStyle = this.borderFill;
-				;
-				ctx.fill();
-			}
-				
-			if(border === .9) //inner vertical bars right
-			{
-				x = 30 * borderIndex;
-				y = 55 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.rect(x+165,y-32,16,40);
+				ctx.fillRect(x-7.5,y-7,16,40);
 				ctx.fillStyle = this.borderFill;
 				
-				ctx.fill();
-				
 			}
-			if(border === 1) //dots
+			if(border === 1)
 			{
-				x = 42 * borderIndex;
+				x = 36 * borderIndex;
 				y = 58 + 20 * rowIndex;
 				ctx.beginPath();
-				ctx.arc(x,y-25,2.5, 0, 3 * Math.PI);
-				ctx.fillStyle = this.borderFill;
-				ctx.closePath();
-				ctx.fill();
-			}
-				if(border === 3) //power pellets
-			{
-				x = 42 * borderIndex;
-				y = 57 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.arc(x,y-25,6, 0, 3 * Math.PI);
+				ctx.arc(x,y,2.5, 0, 3 * Math.PI);
 				ctx.fillStyle = "rgb(255, 255, 255)";
 				ctx.closePath();
 				ctx.fill();
 			}
-			if(border === 4) //pacman
+				if(border === 3)
+			{
+				x = 36 * borderIndex;
+				y = 57 + 20 * rowIndex;
+				ctx.beginPath();
+				ctx.arc(x,y,6, 0, 3 * Math.PI);
+				ctx.fillStyle = "rgb(255, 255, 255)";
+				ctx.closePath();
+				ctx.fill();
+			}
+			if(border === 4)
 			{
 				x = 47 * borderIndex;
 				y = 57 + 20 * rowIndex;
 				ctx.beginPath();
-				ctx.arc(x, y-20, 10, 0.25 * Math.PI, 1.25 * Math.PI, false);
+				ctx.arc(x, y, 10, 0.25 * Math.PI, 1.25 * Math.PI, false);
 				ctx.fillStyle = "rgb(255, 255, 0)";
 				ctx.closePath();
 				ctx.fill();
 				ctx.beginPath();
-				ctx.arc(x, y-20, 10, 0.75 * Math.PI, 1.75 * Math.PI, false);
+				ctx.arc(x, y, 10, 0.75 * Math.PI, 1.75 * Math.PI, false);
 				ctx.closePath();
 				ctx.fill();
 					
 			}
-			if(border === 5) //blinky red ghost
+			if(border === 5)
 			{
 				x = 45 * borderIndex;
 				y = 70 + 20 * rowIndex;	
-				createGhost(ctx, x-38, y-25, "red", "red", "black");
-							
+				createGhost(ctx, x, y, "red", "red", "black");	
 			}
-			if(border === 6) //inky skyblue ghost
+			if(border === 6)
 			{
 				x = 45 * borderIndex;
 				y = 70 + 20 * rowIndex;	
-				createGhost(ctx, x-38,y-25, "skyblue", "skyblue", "black");	
-				
+				createGhost(ctx, x,y, "skyblue", "skyblue", "black");	
 			}
-			if(border === 7) //pinky pink ghost
+			if(border === 7)
 			{
 				x = 45 * borderIndex;
 				y = 70 + 20 * rowIndex;	
-				createGhost(ctx, x-38, y-25, "pink", "pink", "black");
-				
-			}
-			if(border === .7) //clyde orange ghost
-			{
-				x = 45 * borderIndex;
-				y = 70 + 20 * rowIndex;	
-				createGhost(ctx, x-38, y-25, "orange", "orange", "black");
-				
-			}
-			if(border === .2)
-			{
-				x = 47 * borderIndex;
-				y = 57 + 20 * rowIndex;
-				ctx.beginPath();
-				ctx.arc(x-45, y-20, 10, 0.25 * Math.PI, 1.25 * Math.PI, false);
-				ctx.fillStyle = "rgb(255, 255, 0)";
-				ctx.closePath();
-				ctx.fill();
-				ctx.beginPath();
-				ctx.arc(x-45, y-20, 10, 0.75 * Math.PI, 1.75 * Math.PI, false);
-				ctx.closePath();
-				ctx.fill();
-					
+				createGhost(ctx, x, y, "pink", "pink", "black");
 			}
 			
 		})	
